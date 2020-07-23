52cd23f161fcacb1a446cb36695d27d2db08fc3c
AndrewKh7 <rjynhf125>
2020-07-23 15:49:03 +0300

    add tests

--- src/test/java/org/khrapkov/junit1/UserTest.java
+++ src/test/java/org/khrapkov/junit1/UserTest.java
@@ -1,6 +1,8 @@
 package org.khrapkov.junit1;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,80 +11,63 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class UserTest {
+    private User u1;
+    private User u2;
+    private User u3;
 
-    @Test
-    public void howManyUsers() {
-    }
-
-    @Test
-    public void howManyMale() {
-    }
-
-    @Test
-    public void howManyFemale() {
-    }
-
-    @Test
-    public void getSummaryAgeOfUsers() {
+    @Before
+    public void init(){
+        this.u1 = new User("Anna",23, User.Sex.FEMALE);
+        this.u2 = new User("John",25, User.Sex.MALE);
+        this.u3 = new User("Kate",23, User.Sex.FEMALE);
     }
 
     @Test
     public void testGetSummaryAgeOfUsers() {
-    }
-
-    @Test
-    public void getAverageAgeOfUsers() {
+        Assert.assertTrue("getSummaryAgeOfUsers wrong",
+                this.u1.getSummaryAgeOfUsers() == (this.u1.getAge()
+                        + this.u2.getAge()
+                        + this.u3.getAge())
+        );
     }
 
     @Test
     public void testGetAverageAgeOfUsers() {
+        int average = (this.u1.getAge() + this.u3.getAge() + this.u2.getAge()) / 3;
+        Assert.assertTrue("getSummaryAgeOfUsers wrong",
+                this.u1.getAverageAgeOfUsers() == average);
     }
 
     @Test
-    public void getAllUsers() {
-        User u1 = new User("Anna",23, User.Sex.FEMALE);
-        User u2 = new User("John",25, User.Sex.MALE);
-        User u3 = new User("Kate",23, User.Sex.FEMALE);
+    public void allUsersNoNull(){
+       List<User> users = User.getAllUsers();
 
-        List<User> extended = User.getAllUsers();
-
-        List<User> actual = new ArrayList<>();
-        actual.add(u1);
-        actual.add(u2);
-        actual.add(u3);
-
-        Assert.assertEquals(extended,actual);
+       Assert.assertNotNull(users);
     }
 
     @Test
     public void testGetAllUsers() {
+        List<User> expected = User.getAllUsers();
+        List<User> actual = new ArrayList<>();
+        actual.add(u1);
+        actual.add(u2);
+        actual.add(u3);
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void testHowManyUsers() {
+        Assert.assertTrue("quantity of users wrong", this.u1.howManyUsers() == 3);
     }
 
     @Test
     public void testHowManyMale() {
+        Assert.assertTrue("quantity of male wrong", this.u1.howManyMale() == 1);
     }
 
     @Test
     public void testHowManyFemale() {
+        Assert.assertTrue("quantity of female wrong", this.u1.howManyFemale() == 2);
     }
 
-    @Test
-    public void testGetSummaryAgeOfUsers1() {
-    }
-
-    @Test
-    public void testGetSummaryAgeOfUsers2() {
-    }
-
-    @Test
-    public void testGetAverageAgeOfUsers1() {
-    }
-
-    @Test
-    public void testGetAverageAgeOfUsers2() {
-    }
 }
